584883fe960bbbe614b100e230a20d114a5d2270
yuchan1212 <yuchan1212@naver.com>
2021-05-19 00:19:18 +0900

    news_search.py is updated by student1!

--- news_search.py
+++ news_search.py
@@ -4,6 +4,8 @@
 import time
 import selenium
 from selenium import webdriver
+import matplotlib
+import matplotlib.pyplot as plt
 
 print('관련 기사를 찾아드립니다! 검색어를 입력하세요.')
 keyword = input('>> : ')
@@ -19,8 +21,25 @@ search_btn.click()
 
 news_list = driver.find_elements_by_class_name('news_tit')
 
+title_list = []
+index = 0
 for title in news_list:
-    print(title.text)
+    index += 1
+    title_list.append(title.text)
+    print('(%d) ' %index + title.text)
+
+matplotlib.rcParams["font.size"]=14
+plt.rc('font', family='Malgun Gothic')
+
+x = list(range(1,11))
+y = [10,9,8,7,6,5,4,3,2,1]
+plt.bar(x, y, color='b', label='관련도')
+plt.title('검색어 관련 10개 기사의 관련도')
+plt.legend(loc='upper right')
+plt.xlabel('검색어 관련 10개 기사 제목')
+plt.ylabel('관련도(상대적)')
+
+plt.show() # 그래프 창 닫기를 해주셔야 다음 단계가 진행됩니다
 
 print('')
 second = int(input('몇 초 간 기사 화면을 탐색하시겠습니까? '))
